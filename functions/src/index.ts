cb787e45280f4e1dd0abe8a416f8831eae30e973
izumi0x01 <izumisoh.develop@gmail.com>
2022-05-01 16:48:13 +0900

    expressの導入

---
+++ functions/src/index.ts
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+
+const express = require('express');
+
+const app = express();
+app.get('/timestamp', (request, response) => {
+  response.send(`${Date.now()}`);
+})
+
+exports.app = functions.https.onRequest(app);
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
