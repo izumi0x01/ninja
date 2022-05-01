bb59350d0fca6c5d28ac9e1d6e5df31c115b0faa
izumi0x01 <izumisoh.develop@gmail.com>
2022-05-02 01:43:32 +0900

    2022/05/01時点でとりあえずプッシュ

---
+++ components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  
+  <div>
+    hoge
+    <!-- <p @click="onClick">{{ text }}</p> -->
+  </div> 
+</template>
+
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "nuxt-property-decorator";
+
+@Component({
+  name: "Button",
+})
+export default class Button extends Vue {
+  @Prop() private readonly text!: string;
+  @Emit() private onClick(): void {}
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <Tutorial/>
+  <link-text text="なんかurl" @on-click="clickUrl()" />
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component, Prop, Vue } from "nuxt-property-decorator";
 
-export default Vue.extend({})
+import Button from "../components/Button.vue";
+
+
+// @componentは，続けて定義しているクラスをVueが認識できる形式に変換する．
+// つまり，クラスを後ろに必ず定義しないといけない．
+
+//Dataは，クラス定義の中で利用する．
+//computedはクラスのゲッターとして利用する．算出プロパティは，読み取り専用という意味．
+//methodsはmethodsの中で利用する必要はなく，ただ関数としてかけばいいだけ．
+ //pointは，getを付けないといけないのが，算出プロパティ(computed)で，getをつけなくていいのがmethods（関数）
+ //@componentは，引数としてVueのオブジェクトを指定する
+ 
+@Component({
+  name: "ParentComponent",
+  components: {
+    Button,
+  }
+})
+export default class ParentComponent extends Vue {
+  private clickUrl(): void {
+    // なんかクリックされたときの処理
+  }
+  message = ""
+  async mounted(){
+    let res = await firebase.functions().httpsCallable('helloWorld')();
+    this.message = res.data.message;
+ }
+}
 </script>
